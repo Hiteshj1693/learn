42e36ffffc16ae31644ae19c4b89d8240224da33
Hitesh Jethava <Hiteshjethava161611@gmail.com>
2025-01-06 23:44:49 +0530

    edit

--- hit.py
+++ hit.py
@@ -1,1 +1,11 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
 print("hit")
